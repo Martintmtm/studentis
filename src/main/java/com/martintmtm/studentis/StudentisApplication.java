1d35eaf11c4bbf07f4776a9985bb9d12412650e6
martintmtm <martintmtm@gmail.com>
2020-04-13 14:12:23 +0200

    first commit

---
+++ src/main/java/com/martintmtm/studentis/StudentisApplication.java
@@ -0,0 +1,13 @@
+package com.martintmtm.studentis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martintmtm/studentis/StudentisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martintmtm.studentis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
